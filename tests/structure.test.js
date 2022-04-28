2a506478e8bf960f2533a024b99cccfc5c96b8b2
Siarhei Khonski <armonius@mail.ru>
2022-04-28 11:00:47 +0300

    feat: refactor the test for the structure

--- tests/structure.test.js
+++ tests/structure.test.js
@@ -4,22 +4,46 @@ import App from "../src/ts/app/app"
 describe("The app's structure test:", () => {
   test("There is the header in the App:", () => {
     render(App);
-    const header = screen.getByTestId("header");
+    let header = null;
+    try {
+      header = screen.getByTestId("header");
+    }
+    catch {
+      throw new Error("There is no header.")
+    }
     expect(header).not.toBe(null);
   });
   test("There is the navigation in the App:", () => {
     render(App);
-    const nav = screen.getByTestId("nav");
+    let nav = null;
+    try {
+      nav = screen.getByTestId("nav");
+    }
+    catch {
+      throw new Error("There is no nav.")
+    }
     expect(nav).not.toBe(null);
   });
   test("There is the main part in the App:", () => {
     render(App);
-    const main = screen.getByTestId("main");
+    let main = null;
+    try {
+      main = screen.getByTestId("main");
+    }
+    catch {
+      throw new Error("There is no main.");
+    }
     expect(main).not.toBe(null);
   });
   test("There is the footer in the App:", () => {
     render(App);
-    const footer = screen.getByTestId("footer");
+    let footer = null;
+    try {
+      footer = screen.getByTestId("footer");
+    }
+    catch {
+      throw new Error("There is no footer.")
+    } 
     expect(footer).not.toBe(null);
   });
 });
